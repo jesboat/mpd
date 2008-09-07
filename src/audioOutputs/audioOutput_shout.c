d5187ce694cca1c5bd05bd562d9494e7387a86d0
Max Kellermann <max@duempel.org>
2008-09-07 19:14:39 +0200

    fix -Wcast-qual -Wwrite-strings warnings
    The previous patch enabled these warnings.  In Eric's branch, they
    were worked around with a generic deconst_ptr() function.  There are
    several places where we can add "const" to pointers, and in others,
    libraries want non-const strings.  In the latter, convert string
    literals to "static char[]" variables - this takes the same space, and
    seems safer than deconsting a string literal.

--- src/audioOutputs/audioOutput_shout.c
+++ src/audioOutputs/audioOutput_shout.c
@@ -636,8 +636,8 @@ static int myShout_play(AudioOutput * audioOutput,
 
 	for (i = 0; i < samples; i++) {
 		for (j = 0; j < sd->audioFormat->channels; j++) {
-			vorbbuf[j][i] =
-			  (*((mpd_sint16 *)deconst_ptr(playChunk))) / 32768.0;
+			vorbbuf[j][i] = (*((const mpd_sint16 *) playChunk))
+			                 / 32768.0;
 			playChunk += bytes;
 		}
 	}
